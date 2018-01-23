6967399e2f7c931a1518d156bdc6b3297e82400f
EJay <ejay@anyong.com.tw>
2018-01-23 21:03:29 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -15,27 +15,26 @@ import { materialHandler as mh, ImageMaterial } from "./material";
   mh.addMaterial("f", "image", "https://images.pexels.com/photos/767697/pexels-photo-767697.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb")
   mh.addMaterial("g", "image", "https://images.pexels.com/photos/166576/pexels-photo-166576.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb")
   mh.addMaterial("h", "image", "https://images.pexels.com/photos/129753/pexels-photo-129753.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb")
+  let val = 0;
   mh.process.subscribe(p => {
     console.log(`${p} / ${mh.total}`);
+    val = p;
   });
-
-
-  [
-    { key: 'a', value: 'b' },
-    { key: 'c', value: 'd' }
-  ]
-    .reduce((prev, curr) => ({
-      ...prev,
-      [curr.key]: curr.value
-    }), {});
-
-
-
-  await mh.load();
   let fps = 60;
-  let image = mh.materials["test"] as ImageMaterial;
-
+  // let image = mh.materials["test"] as ImageMaterial;
+  setInterval(() => {
+    // canvas.clear();
+    canvas.fullRect("#FFF");
+    canvas.ctx.fillStyle = "#333";
+    let w = canvas.width * .8 / mh.total * val;
+    canvas.ctx.fillRect(canvas.width * .1, canvas.height / 2, w, 6)
+    canvas.ctx.font = "36pt Calibri";
+    canvas.ctx.fillStyle = "#F00";
+    let offset = canvas.ctx.measureText(`${val / mh.total}%`);
+    canvas.ctx.fillText(`${val / mh.total}%`, (canvas.width - offset.width) / 2, canvas.height / 2);
+    // console.log();
+  }, 1000 / fps);
+  await mh.load();
   // let people = new TestPeople();
-
-
+  return;
 })();
--- src/material.ts
+++ src/material.ts
@@ -50,7 +50,7 @@ class MaterialHandler {
       .map(async (name) => {
         let material = this.materials[name];
         await material.loadMaterial();
-        // await this.sleep(Math.random() * 10 * 1000);
+        await this.sleep(Math.random() * 10 * 1000);
         this.process.next(this.process.getValue() + 1);
         return
       });
