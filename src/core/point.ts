6aa832003400b687978ce7d3a93e19b3b52bce29
EJay <ejay@anyong.com.tw>
2018-02-01 22:05:38 +0800

    提供back

--- src/core/point.ts
+++ src/core/point.ts
@@ -1,6 +1,8 @@
 import { Subject } from "rxjs";
 
 export class Point {
+
+  private prev = { x: 0, y: 0 };
   // region x
   private _x: number = 0;
   public get x() {
@@ -11,6 +13,7 @@ export class Point {
     if (typeof this.option.maxX == "number") x = Math.max(x, this.option.maxX);
     if (typeof this.option.minX == "number") x = Math.min(x, this.option.minX);
     if (this._x === x) return;
+    this.prev.x = this.x;
     this._x = x;
     this.pointChange.next(this);
   }
@@ -26,13 +29,13 @@ export class Point {
     if (typeof this.option.maxY == "number") y = Math.max(y, this.option.maxY);
     if (typeof this.option.minY == "number") y = Math.min(y, this.option.minY);
     if (this._y === y) return;
+    this.prev.y = this.y;
     this._y = y;
     this.pointChange.next(this);
   }
   // endregion
 
   public pointChange = new Subject<Point>();
-
   public constructor(x = 0, y = 0, private option: {
     maxX?: number,
     minX?: number,
@@ -52,10 +55,34 @@ export class Point {
       }
       return { x, y };
     }).subscribe(point => {
-      this.x = point.x;
-      this.y = point.y;
+      this.set(point.x, point.y);
     })
     target.pointChange.next(target);
   }
 
+  public set(x: number, y: number) {
+    let isChange = false;
+    if (typeof x == "number") {
+      if (typeof this.option.maxX == "number") x = Math.max(x, this.option.maxX);
+      if (typeof this.option.minX == "number") x = Math.min(x, this.option.minX);
+      if (this._x != x) {
+        this.prev.x = this.x;
+        this._x = x;
+        isChange = true;
+      };
+    }
+    if (typeof y == "number") {
+      if (typeof this.option.maxY == "number") x = Math.max(x, this.option.maxY);
+      if (typeof this.option.minY == "number") x = Math.min(x, this.option.minY);
+      if (this._y != y) {
+        this.prev.y = this.y;
+        this._y = y;
+        isChange = true;
+      };
+    }
+    if (isChange) this.pointChange.next(this);
+  }
+  public back() {
+    this.set(this.prev.x, this.prev.y);
+  }
 }
